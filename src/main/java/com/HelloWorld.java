0386d2390a9bb797c793a2a6a3856c6d6e2d3c1a
samarthgithub <samarth.phadnis@gmail.com>
2019-02-04 16:03:43 +0530

    Initial Commit

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,8 @@
+package com;
+
+public class HelloWorld {
+    public void printName()
+    {
+
+    }
+}
---
+++ src/test/java/com/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @org.junit.Test
+    public void printName() {
+    }
+}
